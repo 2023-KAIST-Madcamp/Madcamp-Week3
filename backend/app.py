d6dd30eca0ac76464fbb6608d61085fda9be19a9
이형진 <dlgudwls84@naver.com>
2024-01-12 16:33:48 +0900

    .

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from gridfs import GridFS
 from bson.objectid import ObjectId
-from flask import Flask, request
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 client = MongoClient('mongodb+srv://dlgudwls8184:NeTSWJRhf3bF7yIe@cluster0.escpqml.mongodb.net/')
@@ -16,11 +16,21 @@ def index():
     if request.method == 'POST':
         data = request.json  # Get the JSON data sent from React Native
         print(data)
-        user_list = list(user_collection.find({'name': data['name']}))
-        print(len(uesr_list))
+
+
+
 
         
-        return 'Data received and updated in MongoDB!'
+        user_list = list(user_collection.find({'kakaoid': data['kakaoid']}))
+        print(user_list)
+        if(len(user_list) != 0):
+            print("이미 존재하는 계정 -> 로그인")
+            print(user_list[0]['_id'])
+            return jsonify({'User_id' : str(user_list[0]['_id'])})
+        else:
+            print("새로운 계정 -> 등록")
+            result = user_collection.insert_one({"kakaoid": data['kakaoid'], "name": data['name']})
+            return jsonify({'user_id' : str(result.inserted_id)})
 
 if __name__ == '__main__':
     app.run(debug=True)
